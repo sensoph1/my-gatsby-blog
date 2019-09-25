6bbb453d5ba76bd34347e98106d7faaf4123d800
sensoph1 <sensoph@gmail.com>
2019-09-25 15:53:33 -0500

    initial

--- config/index.js
+++ config/index.js
@@ -1,16 +1,16 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'JeremyBarkley.com', // Navigation and Site Title
+  siteTitleAlt: 'JeremyBarkley', // Alternative Site title for SEO
+  siteTitleManifest: 'JeremyBarkley',
+  siteUrl: 'https://JeremyBarkley.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Gatsby Blog powered by MDX.', // Your site description
+  author: 'jeremybarkley', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
