6dff1f42718bf8d421de0a82f6d8b4154b5b7734
sensoph1 <sensoph@gmail.com>
2020-10-01 06:46:14 -0500

    updateabout

--- src/pages/about.js
+++ src/pages/about.js
@@ -50,11 +50,11 @@ const Success = () => (
       <Content>
       <h1>About me</h1>
     <p>I’m Jeremy Barkley. I am a Systems admin for OpenVMS and RedHat Linux systems and freelance web
-       developer living in the Texas Panhandle. I have had an interest in web development and design since as far back as high school when I built a website with 
+       developer living in the Texas Panhandle. I have had an interest in web development and design since as far back as high school when I built a website with
        some friends. The technology has changed a lot and that actually makes me very happy.</p>
-        
-    <p>In my free time (when i can find it), I also enjoy creating objects in 3d using Blender 3d, 
-        as well as CrossFit, spending time with family and being outdoors.</p>
+
+    <p>In my free time (when i can find it), I also enjoy creating objects in 3d using Blender 3d,
+        as well as spending time with family and being outdoors.</p>
       </Content>
     </Wrapper>
   </Layout>
